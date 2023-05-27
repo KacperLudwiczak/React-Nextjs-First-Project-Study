fc3bb134ceafd3d77b81a372d99278fcdd1aaa7d
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-27 18:34:55 +0200

    Show Details Button

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://content.r9cdn.net/rimg/dimg/cd/e7/7e96d54c-lm-4326-16c4886210e.jpg?width=1366&height=768&xhint=1697&yhint=2432&crop=true"
+      title="First Meetup"
+      address="Cracow"
+      description="First Meetup description"
+    />
+  );
+}
