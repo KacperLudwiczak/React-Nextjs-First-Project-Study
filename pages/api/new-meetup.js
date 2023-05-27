0437ea83dcbd027860858dbcb136eeae40b8f070
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-27 20:05:58 +0200

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,6 +1,19 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
-    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://casperludwiczak:casperludwiczak@cluster0.bgp0diy.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted" });
   }
 }
