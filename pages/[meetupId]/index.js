598c8ecdf3573f88a1896f0d8eabd938ef0712ee
KacperLudwiczak <kacperludwiczak@o2.pl>
2023-05-27 19:31:57 +0200

    getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,3 +10,37 @@ export default function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: { meetupId: "m1" },
+      },
+      {
+        params: { meetupId: "m2" },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        image:
+          "https://content.r9cdn.net/rimg/dimg/cd/e7/7e96d54c-lm-4326-16c4886210e.jpg?width=1366&height=768&xhint=1697&yhint=2432&crop=true",
+        title: "First Meetup",
+        address: "Cracow",
+        description: "First Meetup description",
+      },
+    },
+    revalidate: 10,
+  };
+}
